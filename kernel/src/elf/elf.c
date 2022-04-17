60361d52e2d9a60ad6703af76339d1e8c45c858c
do1e <dpj.email@qq.com>
2022-04-17 15:54:20 +0800

    start PA2-2

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -38,11 +38,11 @@ uint32_t loader()
 		{
 
 			// remove this panic!!!
-			panic("Please implement the loader");
+			// panic("Please implement the loader");
 
-/* TODO: copy the segment from the ELF file to its proper memory area */
-
-/* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
+			/* TODO: copy the segment from the ELF file to its proper memory area */
+			
+			/* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use */
