d87a48e8cfa103ce99b56784c4c2669dbed3c116
Do1e <dpj.email@qq.com>
2022-05-29 14:32:58 +0800

    fix build_docker

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,2 +1,2 @@
 docker build --network=host -t nju-nemu:latest ./docker
-docker run --network=host -ti --name nju-nemu-container -p 3464:22 -v "$PWD":/root/NJU-nemu:rw --workdir=/root/NJU-nemu nju-nemu:latest /bin/bash
+docker run --network=host -ti --name nju-nemu-container -v "$PWD":/root/NJU-nemu:rw --workdir=/root/NJU-nemu nju-nemu:latest /bin/bash
